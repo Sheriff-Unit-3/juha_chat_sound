a7e133182cc40762b8a626dc7a0d599f64869e35
Sheriff_Unit-3 <210896603+Sheriff-Unit-3@users.noreply.github.com>
2026-05-28 13:21:43 -0500

    Add check for player name

--- init.lua
+++ init.lua
@@ -3,9 +3,11 @@
 
 minetest.register_on_chat_message(function(name, message)
     for _, player in ipairs(minetest.get_connected_players()) do
-        minetest.sound_play("juha_chat_sound_msg", {
-            to_player = player:get_player_name(),
-            gain = 0.5,
-        })
+        if name ~= player:get_player_name() then
+            minetest.sound_play("juha_chat_sound_msg", {
+                to_player = player:get_player_name(),
+                gain = 0.5,
+            })
+        end
     end
 end)
